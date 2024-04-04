435730375bf73280da43f2bde5ce398d62ac051d
onwudiwee <113317064+onwudiwee@users.noreply.github.com>
2024-04-03 20:33:58 -0400

    Add files via upload
    Project 1 - Statistics
    
    I wrote a program to analyze the data from a study about how couples meet and stay together.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// // main.cpp
+// #include "stats.hpp"
+// #include "p1_library.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+//   cout << "hello from main!\n";
+  cout << "Hello World!\n";
+}
---
+++ stats.cpp
@@ -0,0 +1,49 @@
+// stats.cpp
+#include "stats.hpp"
+#include "p1_library.hpp"
+#include <cassert>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<pair<double, int> > summarize(vector<double> v) {
+  assert(false);
+}
+
+int count(vector<double> v) {
+  assert(false);
+}
+
+double sum(vector<double> v) {
+  assert(false);
+}
+
+double mean(vector<double> v) {
+  assert(false);
+}
+
+double median(vector<double> v) {
+  assert(false);
+}
+
+double mode(vector<double> v) {
+  assert(false);
+}
+
+double min(vector<double> v) {
+  assert(false);
+}
+
+double max(vector<double> v) {
+  assert(false);
+}
+
+double stdev(vector<double> v) {
+  assert(false);
+}
+
+double percentile(vector<double> v, double p) {
+  assert(false);
+}
+
